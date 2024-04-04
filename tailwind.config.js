3127cf488caba532ade6015b7fdb03bd000fd365
minimomo14 <93943633+minimomo14@users.noreply.github.com>
2024-04-04 15:12:59 -0400

    create basic layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,37 +5,93 @@ module.exports = {
     // 
     theme: {
       screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       },
       colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-      },
+        transparent: 'transparent',
+        black: '#000',
+        white: '#fff',
+        gray: {
+          100: '#f7fafc',
+          // ...
+          900: '#1a202c',
+        },
       fontFamily: {
         sans: ['Graphik', 'sans-serif'],
         serif: ['Merriweather', 'serif'],
       },
       extend: {
+        boxShadow: {
+          '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+        },
+        dropShadow: {
+          '3xl': '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '4xl': [
+              '0 35px 35px rgba(0, 0, 0, 0.25)',
+              '0 45px 65px rgba(0, 0, 0, 0.15)'
+          ]
+        },
         spacing: {
-          '128': '32rem',
-          '144': '36rem',
+          px: '1px',
+          0: '0',
+          0.5: '0.125rem',
+          1: '0.25rem',
+          1.5: '0.375rem',
+          2: '0.5rem',
+          2.5: '0.625rem',
+          3: '0.75rem',
+          3.5: '0.875rem',
+          4: '1rem',
+          5: '1.25rem',
+          6: '1.5rem',
+          7: '1.75rem',
+          8: '2rem',
+          9: '2.25rem',
+          10: '2.5rem',
+          11: '2.75rem',
+          12: '3rem',
+          14: '3.5rem',
+          16: '4rem',
+          20: '5rem',
+          24: '6rem',
+          28: '7rem',
+          32: '8rem',
+          36: '9rem',
+          40: '10rem',
+          44: '11rem',
+          48: '12rem',
+          52: '13rem',
+          56: '14rem',
+          60: '15rem',
+          64: '16rem',
+          72: '18rem',
+          80: '20rem',
+          96: '24rem',
         },
         borderRadius: {
-          '4xl': '2rem',
-        }
-      }
-    }
+          'none': '0',
+          'sm': '.125rem',
+          DEFAULT: '.25rem',
+          'lg': '.5rem',
+          'full': '9999px',
+        },
+        container: {
+          center: true,
+          padding: {
+            DEFAULT: '1rem',
+            sm: '2rem',
+            lg: '4rem',
+            xl: '5rem',
+            '2xl': '6rem',
+          },
+        },
+      },
+    },
   },
   plugins: [],
-  
+}
 }
